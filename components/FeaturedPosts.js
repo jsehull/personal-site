d91d2737946422151e26d5f6fa46b0128b389a55
Jesse Hull <jse.hull@gmail.com>
2020-08-11 15:21:50 -0600

    fix silly typo

--- components/FeaturedPosts.js
+++ components/FeaturedPosts.js
@@ -19,7 +19,7 @@ const FeaturedPosts = () => {
   return (
     <PostBox>
       <a href='/blog/my-simple-case-for-standard-js'>
-        <h3>Writing for an Audience of One</h3>
+        <h3>My Simple Case for Standard JS</h3>
       </a>
       <a href='/blog/writing-for-an-audience-of-one'>
         <h3>Writing for an Audience of One</h3>
